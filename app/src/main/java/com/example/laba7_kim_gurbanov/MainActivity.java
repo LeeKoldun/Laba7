6605970cc55388bb032909a4ac8db7bbf0e0913c
LeeKoldun <leadkoldun@gmail.com>
2022-11-01 19:08:35 +0300

    Законченная версия

--- app/src/main/java/com/example/laba7_kim_gurbanov/MainActivity.java
+++ app/src/main/java/com/example/laba7_kim_gurbanov/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.laba7_kim_gurbanov;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void click(View v) {
+        AppCompatButton Btn = (AppCompatButton) v;
+        String txt = Btn.getText().toString();
+        TextView Text = findViewById(R.id.Txt);
+
+        Text.setText("Была нажаа кнопка " + txt);
     }
 }
